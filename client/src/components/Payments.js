fcd2437d1e746976371b6cb94f3bb7fd136370df
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-19 20:31:47 +0300

    Add billing and credit to user model

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,15 @@
  * Date    : 19/10/2017
  */
 import React, { Component } from 'react';
-import StripeCheckout from 'react-stripe-checkout'
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 
 /**
  * StripeCheckout Properties/Options
+ * name: Like title that appears in the payment form popup
+ * description: appears below name in the payment form popup
  * amount: # of US cents, e.g. 500 = 5 US dollars
  * token: the token we receive from Stripe
  * stripeKey:the Publishable Stripe API key
@@ -18,12 +23,18 @@ class Payments extends Component {
 
     return (
       <StripeCheckout
+        name="FeedColla"
+        description="$5 for 5 email credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
